f797bf22c12bfb000e17e65c550d543345347b58
jhyunwoo <tiers.edamame0z@icloud.com>
2022-04-13 03:47:45 +0900

    change character

--- next.config.js
+++ next.config.js
@@ -4,9 +4,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-const withImages = require("next-images");
-module.exports = withImages({
-  webpack(config, options) {
-    return config;
-  },
-});
